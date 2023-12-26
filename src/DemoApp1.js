b4c63f638ed33bff34fdb8b8eefa3aa18e9eb8d0
hanh <hanh@gmail.com>
2023-12-26 20:42:24 +0700

    a

---
+++ src/DemoApp1.js
@@ -0,0 +1,10 @@
+import React from "react";
+class DemoApp1 extends React.Component{
+  render(){
+    return(
+      <h1> chao ca nha</h1>
+    )
+  }
+}
+
+export default DemoApp1
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import DemoApp1 from './DemoApp1';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DemoApp1 />
   </React.StrictMode>
 );
 
